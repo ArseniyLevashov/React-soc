0ee4e18ebc20405bed7e2c06c7ee949f4fcd77bc
ArseniyLevashov <59114768+ArseniyLevashov@users.noreply.github.com>
2020-05-14 15:23:50 +0300

    Update Header.jsx

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,7 +3,12 @@ import s from "./Header.module.css";
 
 const Header = () =>{
     return <header className={s.header}>
-    <img src="#"></img>
+    <div className={s.lk_container}>
+    <img src="https://cdn.iconscout.com/icon/free/png-512/react-1-282599.png"></img>
+    <a href="#">Learn React</a>
+    <a href="https://reactjs.org/">Home</a>
+    <a href="https://github.com/ArseniyLevashov">My Github</a>
+    </div>
   </header>
 }
 
